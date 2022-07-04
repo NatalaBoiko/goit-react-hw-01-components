476be08f02c70d43bd8bfa852b2a3becdc7b7f95
NatalaBoiko <natalabojko@gmail.com>
2022-07-04 16:12:33 +0300

    stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
 import user from './Data/user.json';
+import data from './Data/data.json';
 
 export const App = () => {
   return (
@@ -11,6 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,45 @@
-// import data from '../Data/data.json';
-// import css from './Ststistic.module.css';
+import css from './Ststistics.module.css';
+import propTypes from 'prop-types';
 
-// export const Statistics = () => {
-//   return (
-//     <section className="statistics">
-//       <h2 className="title">Upload stats</h2>
+const createColor = () => {
+  const color =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    0.5 +
+    ')';
 
-//       <ul className="stat-list">
-//         <li className="item">
-//           <span className="label">.docx</span>
-//           <span className="percentage">4%</span>
-//         </li>
-//         <li className="item">
-//           <span className="label">.mp3</span>
-//           <span className="percentage">14%</span>
-//         </li>
-//         <li className="item">
-//           <span className="label">.pdf</span>
-//           <span className="percentage">41%</span>
-//         </li>
-//         <li className="item">
-//           <span className="label">.mp4</span>
-//           <span className="percentage">12%</span>
-//         </li>
-//       </ul>
-//     </section>
-//   );
-// };
+  return color;
+};
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.stats__list}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.stats}
+              key={id}
+              style={{ backgroundColor: createColor() }}
+            >
+              <span class={css.label}>{label}</span>
+              <span class={css.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  label: propTypes.string.isRequired,
+  percentage: propTypes.number.isRequired,
+};
